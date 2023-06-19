da10d6ebe839fae0f96838fc76c45f6411196b49
PabloNavarroRodriguez <102168490+PabloNavarroRodriguez@users.noreply.github.com>
2023-06-19 22:34:16 +0200

    Assets

---
+++ src/assets/scripts/funciones_globales.js
@@ -0,0 +1,8 @@
+function desplegarMenu() {
+  var menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "none" ? "block" : "none";
+}
+
+function abrirPDF() {
+  window.open('../pdfs/CVDEFINITIVO (1).pdf', '_self');
+}
